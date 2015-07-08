45cabd96f74af11c895b957ac9c2e8b73b46fa39
Alessandro Vincelli <a.vincelli@gmail.com>
2015-07-08 23:24:47 +0200

    cuatomers

---
+++ av-common-dao/src/main/java/it/av/apps/model/cargo/Customer.java
@@ -0,0 +1,129 @@
+package it.av.apps.model.cargo;
+
+import it.av.apps.model.BasicEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customers", indexes = { @Index(name = "idx_customers_companyName", columnList = "companyName") })
+public class Customer extends BasicEntity {
+
+    public static final String COMPANY_NAME  = "companyName";
+    
+    private static final long serialVersionUID = 1L;
+
+    private String companyName;
+    private String companyName2;
+    private String address;
+    private String country;
+    private String city;
+    private String type;
+    private String languague;
+    private String currency;
+    private String fiscalCode;
+    private String vatCode;
+    private String refFirstName;
+    private String refLastName;
+    private String refPhone1;
+    private String refPhone2;
+    private String refEmail;
+    
+    public String getCompanyName() {
+        return companyName;
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    public String getCompanyName2() {
+        return companyName2;
+    }
+    public void setCompanyName2(String companyName2) {
+        this.companyName2 = companyName2;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getLanguague() {
+        return languague;
+    }
+    public void setLanguague(String languague) {
+        this.languague = languague;
+    }
+    public String getCurrency() {
+        return currency;
+    }
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+    public void setFiscalCode(String fiscalCode) {
+        this.fiscalCode = fiscalCode;
+    }
+    public String getVatCode() {
+        return vatCode;
+    }
+    public void setVatCode(String vatCode) {
+        this.vatCode = vatCode;
+    }
+    public String getRefFirstName() {
+        return refFirstName;
+    }
+    public void setRefFirstName(String refFirstName) {
+        this.refFirstName = refFirstName;
+    }
+    public String getRefLastName() {
+        return refLastName;
+    }
+    public void setRefLastName(String refLastName) {
+        this.refLastName = refLastName;
+    }
+    public String getRefPhone1() {
+        return refPhone1;
+    }
+    public void setRefPhone1(String refPhone1) {
+        this.refPhone1 = refPhone1;
+    }
+    public String getRefPhone2() {
+        return refPhone2;
+    }
+    public void setRefPhone2(String refPhone2) {
+        this.refPhone2 = refPhone2;
+    }
+    public String getRefEmail() {
+        return refEmail;
+    }
+    public void setRefEmail(String refEmail) {
+        this.refEmail = refEmail;
+    }
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+    
+    
+
+}
